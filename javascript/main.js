a64599bfe5b4f1562caa6e4e17e27d59d508f0ba
Nidhi-Sharma-csit <nidhi1912002@akgec.ac.in>
2020-08-25 19:29:37 +0530

    In html: added images, gave proper class name and id to those images.

--- javascript/main.js
+++ javascript/main.js
@@ -1,46 +1,199 @@
 document.addEventListener('DOMContentLoaded',function(){
 
-    function myMove(){
-        var elem = document.getElementById('child');
+    function myMove1(elem_id){
+        var elem = document.getElementById(elem_id);
         var pos = 0;
-        var h = 0;
-        var inc = -1;
-        var id= setInterval(frame, 5);
-        var id2= setInterval(elemHeight, 5);
+        var h = 250;
+        var in1= -1;
+        var in2 = -1;
+        var id= setInterval(frame, 3);
+        var id2= setInterval(elemHeight, 2);
         function elemHeight(){
-            
-            if( inc<0)
+            if( in2<0)
             {
                 h++;
-                elem.style.bottom = h+'px';
-                if( h == 15){
-                    inc = inc*-1;
+                if( h == 260){
+                    in2 = in2*-1;
                 }
             }
             else{
                 h--;
-                elem.style.bottom = h+'px';
-                if( h==0){
-                    inc = inc*-1;
+                if( h==240){
+                    in2 = in2*-1;
                 }
             }
-
+            elem.style.bottom = h+'px';
         }
         function frame(){
-            if(pos == 50){
-                elem.setAttribute("src","images/jerry running.png");
-                elem.style.height = '200px';
-                elem.style.width = '200px';
-            }
-            if(pos==1000){
-                pos = 0
-               // clearInterval(id);
+
+            if( in1<0){
+                pos++;
+                if( pos == 300){
+                    in1 = in1*-1;
+                }
             }
             else{
-                pos++;
-                elem.style.left = pos+'px';
+                pos--;
+                if( pos == 0){
+                    in1 = in1*-1;
+                }
+            }
+            elem.style.left = pos + 'px';
+        }
+ }
+ //////////////////////////////////////////////////////////////////////////////////////////////////////////
+ function myMove2(elem_id){
+    var elem = document.getElementById(elem_id);
+    var pos = 0;
+    var h = 0;
+    var in1= -1;
+    var in2 = -1;
+    var id= setInterval(frame, 3);
+    function frame(){
+        if( in1<0)
+        {
+            pos++;
+            if( pos == 275){
+                in1 = in1*-1;
             }
         }
+         else{
+             pos--;
+             if( pos == 0){
+                 in1 = in1*-1;
+            }
+        }
+        elem.style.left = pos + 'px';
+        elem.style.bottom = pos + 'px';
+    }  
+}
+//////////////////////////////////////////////////////////////////////////////////////////////
+function myMove3(elem_id){
+    var elem = document.getElementById(elem_id);
+    var pos = 0;
+    var h = 0;
+    var in1= -1;
+    var in2 = -1;
+    var id= setInterval(frame, 3);
+    function frame(){
+        if( in1<0)
+        {
+            pos++;
+            if( pos == 275){
+                in1 = in1*-1;
+            }
+        }
+         else{
+             pos--;
+             if( pos == 0){
+                 in1 = in1*-1;
+            }
+        }
+        elem.style.left = pos + 'px';
+        elem.style.top = pos + 'px';
+    }  
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+function myMove4(elem_id){
+    var elem = document.getElementById(elem_id);
+    var pos = 0;
+    var h = 250;
+    var in1= -1;
+    var in2 = -1;
+    var id= setInterval(frame, 3);
+    var id2= setInterval(elemHeight, 2);
+    function elemHeight(){
+        if( in2<0)
+        {
+            h++;
+            if( h == 260){
+                in2 = in2*-1;
+            }
+        }
+        else{
+            h--;
+            if( h==240){
+                in2 = in2*-1;
+            }
+        }
+        elem.style.bottom = h+'px';
     }
-    myMove();
+    function frame(){
+        if( in1<0){
+            pos++;
+            if( pos == 300){
+                in1 = in1*-1;
+            }
+        }
+        else{
+            pos--;
+            if( pos == 0){
+                in1 = in1*-1;
+            }
+        }
+        elem.style.right = pos + 'px';
+    }
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function myMove5(elem_id){
+    var elem = document.getElementById(elem_id);
+    var pos = 0;
+    var in1= -1;
+    var id= setInterval(frame, 3);
+    function frame(){
+        if( in1<0)
+        {
+            pos++;
+            if( pos == 275){
+                in1 = in1*-1;
+            }
+        }
+         else{
+             pos--;
+             if( pos == 0){
+                 in1 = in1*-1;
+            }
+        }
+        elem.style.right = pos + 'px';
+        elem.style.bottom = pos + 'px';
+    }  
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+function myMove6(elem_id){
+    var elem = document.getElementById(elem_id);
+    var pos = 0;
+    var in1= -1;
+    var id= setInterval(frame, 3);
+    function frame(){
+        if( in1<0)
+        {
+            pos++;
+            if( pos == 275){
+                in1 = in1*-1;
+            }
+        }
+         else{
+             pos--;
+             if( pos == 0){
+                 in1 = in1*-1;
+            }
+        }
+        elem.style.right = pos + 'px';
+        elem.style.top = pos + 'px';
+    }  
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    myMove1('c1');
+    myMove2('c2');
+    myMove3('c3');
+    myMove4('c4');
+    myMove5('c5');
+    myMove6('c6');
+    myMove1('c7');
+    myMove2('c8');
+    myMove3('c9');
+    myMove4('c10');
+    myMove5('c11');
+    myMove6('c12');
 });
