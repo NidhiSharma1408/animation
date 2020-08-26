1239295b7e63a39757537bd422bcd931fa4ca186
Nidhi-Sharma-csit <nidhi1912002@akgec.ac.in>
2020-08-26 16:38:56 +0530

    changed size of corona images and decreased speed of motion

--- javascript/main.js
+++ javascript/main.js
@@ -1,31 +1,29 @@
 document.addEventListener('DOMContentLoaded',function(){
-
-    function myMove1(elem_id){
+    function myMove1(elem_id,time,high){
         var elem = document.getElementById(elem_id);
         var pos = 0;
-        var h = 250;
+        var h = high;
         var in1= -1;
         var in2 = -1;
-        var id= setInterval(frame, 3);
-        var id2= setInterval(elemHeight, 2);
+        var id= setInterval(frame, time);
+        var id2= setInterval(elemHeight, time);
         function elemHeight(){
             if( in2<0)
             {
                 h++;
-                if( h == 260){
+                if( h == high+10){
                     in2 = in2*-1;
                 }
             }
             else{
                 h--;
-                if( h==240){
+                if( h==high-10){
                     in2 = in2*-1;
                 }
             }
             elem.style.bottom = h+'px';
         }
         function frame(){
-
             if( in1<0){
                 pos++;
                 if( pos == 300){
@@ -42,25 +40,24 @@ document.addEventListener('DOMContentLoaded',function(){
         }
  }
  //////////////////////////////////////////////////////////////////////////////////////////////////////////
- function myMove2(elem_id){
+ function myMove2(elem_id,time){
     var elem = document.getElementById(elem_id);
     var pos = 0;
     var h = 0;
     var in1= -1;
     var in2 = -1;
-    var id= setInterval(frame, 3);
+    var id= setInterval(frame, time);
     function frame(){
-        if( in1<0)
-        {
+        if( in1<0){
             pos++;
             if( pos == 275){
                 in1 = in1*-1;
             }
         }
-         else{
-             pos--;
-             if( pos == 0){
-                 in1 = in1*-1;
+        else{
+            pos--;
+            if( pos == 0){
+                in1 = in1*-1;
             }
         }
         elem.style.left = pos + 'px';
@@ -68,13 +65,13 @@ document.addEventListener('DOMContentLoaded',function(){
     }  
 }
 //////////////////////////////////////////////////////////////////////////////////////////////
-function myMove3(elem_id){
+function myMove3(elem_id,time){
     var elem = document.getElementById(elem_id);
     var pos = 0;
     var h = 0;
     var in1= -1;
     var in2 = -1;
-    var id= setInterval(frame, 3);
+    var id= setInterval(frame,time);
     function frame(){
         if( in1<0)
         {
@@ -83,10 +80,10 @@ function myMove3(elem_id){
                 in1 = in1*-1;
             }
         }
-         else{
-             pos--;
-             if( pos == 0){
-                 in1 = in1*-1;
+        else{
+            pos--;
+            if( pos == 0){
+                in1 = in1*-1;
             }
         }
         elem.style.left = pos + 'px';
@@ -94,25 +91,24 @@ function myMove3(elem_id){
     }  
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
-function myMove4(elem_id){
+function myMove4(elem_id,time,high){
     var elem = document.getElementById(elem_id);
     var pos = 0;
-    var h = 250;
+    var h = high;
     var in1= -1;
     var in2 = -1;
-    var id= setInterval(frame, 3);
-    var id2= setInterval(elemHeight, 2);
+    var id= setInterval(frame, time);
+    var id2= setInterval(elemHeight, time);
     function elemHeight(){
-        if( in2<0)
-        {
+        if( in2<0){
             h++;
-            if( h == 260){
+            if( h == high+10){
                 in2 = in2*-1;
             }
         }
         else{
             h--;
-            if( h==240){
+            if( h==high-10){
                 in2 = in2*-1;
             }
         }
@@ -135,11 +131,11 @@ function myMove4(elem_id){
     }
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-function myMove5(elem_id){
+function myMove5(elem_id,time){
     var elem = document.getElementById(elem_id);
     var pos = 0;
     var in1= -1;
-    var id= setInterval(frame, 3);
+    var id= setInterval(frame, time);
     function frame(){
         if( in1<0)
         {
@@ -159,11 +155,11 @@ function myMove5(elem_id){
     }  
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
-function myMove6(elem_id){
+function myMove6(elem_id,time){
     var elem = document.getElementById(elem_id);
     var pos = 0;
     var in1= -1;
-    var id= setInterval(frame, 3);
+    var id= setInterval(frame, time);
     function frame(){
         if( in1<0)
         {
@@ -172,10 +168,10 @@ function myMove6(elem_id){
                 in1 = in1*-1;
             }
         }
-         else{
-             pos--;
-             if( pos == 0){
-                 in1 = in1*-1;
+        else{
+            pos--;
+            if( pos == 0){
+                in1 = in1*-1;
             }
         }
         elem.style.right = pos + 'px';
@@ -184,16 +180,22 @@ function myMove6(elem_id){
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    myMove1('c1');
-    myMove2('c2');
-    myMove3('c3');
-    myMove4('c4');
-    myMove5('c5');
-    myMove6('c6');
-    myMove1('c7');
-    myMove2('c8');
-    myMove3('c9');
-    myMove4('c10');
-    myMove5('c11');
-    myMove6('c12');
+    myMove1('c1',10,300);//horizontal
+    myMove2('c2',9);//to top-right
+    myMove3('c3',8);//to bottom-right
+    myMove4('c4',9,300);//horizontal 
+    myMove5('c5',7);//to top-left
+    myMove6('c6',6);//to bottom-left
+    myMove1('c7',5,250);//horizontal
+    myMove2('c8',6);//to top-right
+    myMove3('c9',10);//to bottom-right
+    myMove4('c10',9,250);//horizontal
+    myMove5('c11',8);//to top-left
+    myMove6('c12',4);//to bottom
+    myMove1('c13',9,200);//horizontal
+    myMove2('c14',7);//to top-right
+    myMove3('c15',7);//to bottom-right
+    myMove4('c16',8,200);//horizontal
+    myMove5('c17',9);//to top-left
+    myMove6('c18',10);//to bottom-left
 });
